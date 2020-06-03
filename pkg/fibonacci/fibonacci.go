903909571d0555c0c35fc5b7757c1389804856a4
Eric K <eric.koehli@mail.utoronto.ca>
2020-06-02 23:49:36 -0400

    Renamed goalgs to fibonacci

--- pkg/fibonacci/fibonacci.go
+++ pkg/fibonacci/fibonacci.go
@@ -2,9 +2,27 @@ package fibonacci
 
 import ()
 
-func Fibonacci(n int) int {
+
+// Recursive Fibonacci function
+func Fib(n int) int {
 	if (n <= 1) {
 		return n;
 	}
-	return Fibonacci(n - 1) + Fibonacci(n - 2);
+	return Fib(n - 1) + Fib(n - 2);
+}
+
+// Iterative dynamic programming fibonacci function
+func MemoizedFib(n int) int {
+	if (n <= 1) {
+		return n;
+	}
+
+	var arr []int = []int{0, 1}						// arr[0] = 0, arr[1] = 1
+	// var arr = []int{0, 1}							// this also works
+
+	for i := 2; i <= n; i++ {
+		arr = append(arr, arr[i - 1] + arr[i - 2]);
+	}
+
+	return arr[n];
 }
